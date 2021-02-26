b1c587ee2ae8049ea110491045efe4a71e9b491b
Anwesh43 <anweshthecool0@gmail.com>
2021-02-27 02:23:06 +0530

    adding Canvas extension methods

--- linkedlinetoendboxview/src/main/java/com/example/linkedlinetoendboxview/LinedToEndBoxView.kt
+++ linkedlinetoendboxview/src/main/java/com/example/linkedlinetoendboxview/LinedToEndBoxView.kt
@@ -29,3 +29,36 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawLinedToEndBox(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    val lineW : Float = w - 2 * size
+    save()
+    translate(w / 2, h / 2)
+    rotate(90f * sf3)
+    save()
+    translate(-lineW / 2, -h / 2 + h * 0.5f * sf1)
+    drawLine(0f, 0f, lineW, 0f, paint)
+    restore()
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f - 2 * j)
+        translate(-w / 2 + size / 2, 0f)
+        drawRect(RectF(-size * 0.5f * sf2, -size * 0.5f * sf2, size * sf2, size * sf3), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawLTEBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLinedToEndBox(scale, w, h, paint)
+}
