7df19321496793b6876eb17e43e00cfde59acdd3
Anwesh43 <anweshthecool0@gmail.com>
2021-02-27 03:48:09 +0530

    adding view in activity

--- app/src/main/java/com/example/linked/MainActivity.kt
+++ app/src/main/java/com/example/linked/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linked
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.linkedlinetoendboxview.LinedToEndBoxView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LinedToEndBoxView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
